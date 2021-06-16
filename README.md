450f4b972aefe985aec7ebb2cd006e3d26253db8
Shane Segui <shanesegui@Shanes-iMac.local>
2021-06-16 16:23:48 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,10 +1,52 @@
-# Getting Started with Create React App
+# react-firebase-sample
+
+A quick and dirty sample React app to explore [Google's Firebase](https://firebase.google.com/) services.
+
+* Firebase Authentication
+* Firebase Realtime Database
+* Firebase Cloud Storage
+* Firebase Cloud Messaging (not yet)
+
+I wanted to add FCM but there is still limitation in browser support.
+
+## Description
+The app is like an online bulletin board. 
+If the user is signed in, they can upload text and image.
+
+**Note:** The config.js is not included in the /src folder for obvious reasons.
+
+Sample format of config.js for reference:
+```
+export const config = {
+    apiKey: "Abc12Defg34HijKlmn567",
+    authDomain: "project-site.firebaseapp.com",
+    projectId: "project-site",
+    storageBucket: "project-site.appspot.com",
+    messagingSenderId: "1234567890",
+    appId: "1:1234567:web:s283j38dhh3384hf",
+    databaseURL: "https://project-site-default-rtdb.firebaseio.com",
+    measurementId: "A-XYZABC123A"
+}
+```
+
+## Installation
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
-
 In the project directory, you can run:
+
+### `npm install`
+
+Installs all required modules and dependencies. 
+
+You also need to install [Firebase CLI](https://firebase.google.com/docs/cli/).
+
+### `firebase init`
+
+To initialize firebase in the app and generate appropriate files and folders like:
+- .firebase/
+- .firebaserc
+- firebase.json
 
 ### `npm start`
 
@@ -14,10 +56,7 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+**Important:** You also need to set the appropriate permission in the rules in Firebase control panel.
 
 ### `npm run build`
 
@@ -28,43 +67,3 @@ The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,203 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/storage";
+import "firebase/auth";
+//import "firebase/messaging";
+
+import {
+  FirebaseDatabaseProvider,
+  FirebaseDatabaseNode
+} from "@react-firebase/database";
+
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  //IfFirebaseAuthedAnd
+} from "@react-firebase/auth";
+
+import { config } from "./config";
+import storage from './storage';
+
+import Login, { LoginModes, LoginTypes  } from './components/Login';
+import AddButton from './components/AddButton';
+import AddPanel from './components/AddPanel';
+import ThumbList from './components/ThumbList';
+import classes from './App.module.css';
+
+function getDateFormat() {
+
+    var odate = new Date();
+
+    var year = odate.getFullYear();
+    var month = odate.getMonth() + 1;
+    var date = odate.getDate();
+
+    var hour = parseInt(odate.getHours());
+    var mins = parseInt(odate.getMinutes());
+    var sec = parseInt(odate.getSeconds());
+
+    hour = hour < 10 ? "0" + hour : hour;
+    mins = mins < 10 ? "0" + mins : mins;
+    sec = sec < 10 ? "0" + sec : sec;
+    
+    var sdate = [year, month, date].join("-");
+    var stime = [hour,mins,sec].join(":");
+
+    return [sdate, stime].join(" ");
+
+}
+
+function getSimpleId() {
+    return ["abc", Date.now()].join("");
+}
+
+export const DIR_DATABASE = "bboard";
+export const DIR_CLOUD_STORAGE = "images";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [isAdd, setIsAdd] = useState(false)
+    
+    const handleLogin = (stype) => {
+        
+        if(stype === LoginTypes.Anonymous) {
+
+            firebase.auth().signInAnonymously()
+
+        } else if(stype === LoginTypes.Google) {
+
+            const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+            firebase.auth().signInWithPopup(googleAuthProvider)
+
+        } else {
+
+            firebase.auth().signOut()
+
+        }
+
+    }
+
+    const handleAdd = () => {
+        
+        setIsAdd(!isAdd)
+
+    }
+
+    const handleCancel = () => {
+
+        setIsAdd(!isAdd)
+
+    }
+
+    const handleSubmit = (caption, image) => {
+
+        const scaption = caption.length > 0 ? caption.trim() : "";
+        
+        if(scaption.length === 0) {
+
+            return;
+
+        }
+
+        const sdate = getDateFormat()
+        const skey = getSimpleId()
+        
+        if(image) {
+
+            const sname = [skey, image.name].join("_");
+            
+            //storage.ref(`/images/${fname}`).put(image)
+            //.on("state_changed", console.log("success!"), console.log)
+            
+            const task = storage.ref(`/${DIR_CLOUD_STORAGE}/${sname}`).put(image);
+            
+            task.on('state_changed', ts => {
+
+                console.log("success!")
+
+            })
+
+            task.then(() => {
+
+                firebase.database().ref(`${DIR_DATABASE}/${skey}`).set({
+                    date: sdate,
+                    caption: scaption,
+                    image: sname,
+                })
+                
+            })
+
+        } else {
+
+            firebase.database().ref(`${DIR_DATABASE}/${skey}`).set({
+                date: sdate,
+                caption: scaption,
+            })
+
+        }
+
+        setIsAdd(!isAdd)
+
+    }
+    
+    return (
+        <div className={classes.container}>
+            <FirebaseAuthProvider firebase={firebase} {...config}>
+                <div className={classes.header}>
+                    <FirebaseAuthConsumer>
+                    {
+                        ({ isSignedIn, user, providerId }) => {
+                            if(isSignedIn) {
+                                return (
+                                    <Login mode={LoginModes.LoggedIn} onClick={handleLogin} />
+                                )
+                            } else {
+                                return (
+                                    <Login mode={LoginModes.LoggedOut} onClick={handleLogin} />
+                                )
+                            }
+                        }
+                    }
+                    </FirebaseAuthConsumer>
+                </div>
+                <div className={classes.main}>
+                    <div className={classes.inner}>
+                        <IfFirebaseAuthed>
+                        {
+                            () => {
+                                return (
+                                    <div className={classes.item}>
+                                        <div className={classes.itemInner}>
+                                            <AddButton onClick={handleAdd} />
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        }
+                        </IfFirebaseAuthed>
+                        <FirebaseDatabaseProvider firebase={firebase} {...config}>
+                            <FirebaseDatabaseNode
+                            path={`${DIR_DATABASE}/`}
+                            >
+                            {
+                                d => <ThumbList data={d} />
+                            }
+                            </FirebaseDatabaseNode>
+                        </FirebaseDatabaseProvider>
+                    </div>
+                </div>
+                <AddPanel 
+                onSubmit={handleSubmit}
+                onCancel={handleCancel} 
+                show={isAdd}
+                />
+            </FirebaseAuthProvider>
+        </div>
+    )
 }
 
 export default App;
